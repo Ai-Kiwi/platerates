688503795d0a42281946d14849eb7b50b115fd32
Ai-Kiwi <contact@aikiwi.dev>
2023-07-08 14:16:17 +1200

    removed test, added base for reating

---
+++ lib/postRating/postRatingList.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PostRatingList extends StatefulWidget {
+  final String postId;
+
+  const PostRatingList({super.key, required this.postId});
+
+  @override
+  _PostRatingListState createState() => _PostRatingListState();
+}
+
+class _PostRatingListState extends State<PostRatingList> {
+  @override
+  Widget build(BuildContext context) {
+    return;
+  }
+}
