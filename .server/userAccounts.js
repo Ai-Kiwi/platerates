92c8887ed933a471caa8ea2e5dca7c4c3f177aea
Ai-Kiwi <contact@aikiwi.dev>
2023-07-17 16:35:59 +1200

    fixed up some bugs

--- .server/userAccounts.js
+++ .server/userAccounts.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const crypto = require('crypto');
 const { database } = require('./database');
 const { testToken } = require('./userLogin');
 const { generateRandomString } = require('./utilFunctions');
