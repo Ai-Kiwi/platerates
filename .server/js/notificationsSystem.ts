92a3bc3662773b9eeaf3eb5ebae108c599b863b7
Ai-Kiwi <contact@aikiwi.dev>
2023-08-15 20:55:50 +1200

    limited width for website

---
+++ .server/js/notificationsSystem.ts
@@ -0,0 +1,12 @@
+async function sendNotification(userId : string,data : Array<any>) {
+    //this function will be ran even if they have notification for it disabled
+
+
+    try{
+
+    }catch (err){
+        return {
+            success : false,
+        }
+    }
+}
