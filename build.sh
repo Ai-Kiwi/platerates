f38df512283a71ae2b130bcde0af7f3e171af617
Ai-Kiwi <contact@aikiwi.dev>
2023-08-16 12:47:27 +1200

    updated to allow different loctions for build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-cd /home/aikiwi/Projects/phoneApps/toaster/
+BRANCH="new-ui"
+
+#already know start dir
 
 echo " # building web"
 flutter build web
@@ -10,7 +12,7 @@ flutter build apk --release
 echo "done"
 /bin/sleep 2
 
-cd /home/aikiwi/Projects/phoneApps/toaster/.server/
+cd .server
 
 sudo systemctl start docker
 
@@ -20,26 +22,32 @@ echo "done"
 /bin/sleep 2
 
 echo " # injecting builds"
+cd ..
 #after build finished bring over files for web and what not
 #only done now and wihtout sys link as not wanted to be included in typescript
-cp -R /home/aikiwi/Projects/phoneApps/toaster/build/web /home/aikiwi/Projects/phoneApps/toaster/.server/web/
-cp /home/aikiwi/Projects/phoneApps/toaster/build/app/outputs/flutter-apk/app-release.apk /home/aikiwi/Projects/phoneApps/toaster/.server/app-release.apk
+cp -R ./build/web ./.server/web/
+cp ./build/app/outputs/flutter-apk/app-release.apk ./toaster/.server/app-release.apk
 
 
 
-echo " # clearing old docker image" 
-sudo docker image rm toaster-server
+cd .server
+
 echo " # building docker image" 
-sudo docker build -t aikiwi1/toaster:latest . 
+sudo docker build -t aikiwi1/toaster:$BRANCH . 
 echo "done"
 /bin/sleep 2
+
 echo " # logging into docker account" 
 sudo docker login
+
 echo " # pushing docker update"
-sudo docker push aikiwi1/toaster:latest
+sudo docker push aikiwi1/toaster:$BRANCH
+echo "done"
+/bin/sleep 2
 
 echo " # showing local docker images" 
 sudo docker images
+/bin/sleep 2
 
 
 #clean up after finished
@@ -49,6 +57,7 @@ rm /home/aikiwi/Projects/phoneApps/toaster/.server/app-release.apk
 
 echo "opening server updating docker install"
 firefox -new-tab "https://192.168.0.166:9443/"
+echo "done, you can now rebuild container"
 
 #to start
 #sudo docker run toaster-server -p 3030:3030
