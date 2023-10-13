39ef1e81af010d7827b45882b8472471435f4d39
Ai-Kiwi <contact@aikiwi.dev>
2023-10-13 22:01:12 +1300

    added controll for pages

--- lib/userFeed/userFeed.dart
+++ lib/userFeed/userFeed.dart
@@ -1,5 +1,6 @@
 import 'package:Toaster/libs/lazyLoadPage.dart';
 import 'package:flutter/material.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
 class userFeed extends StatefulWidget {
   @override
@@ -7,23 +8,62 @@ class userFeed extends StatefulWidget {
 }
 
 class _UserFeedState extends State<userFeed> {
+  String pageOpen = "popular";
+  int pageItemIndex = 0;
+
+  void changePageOpen(index) {
+    setState(() {
+      pageItemIndex = index;
+      if (index == 0) {
+        pageOpen = "popular";
+      } else {
+        pageOpen = "followers";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     //final ThemeData theme = Theme.of(context);
-    return const LazyLoadPage(
+    return LazyLoadPage(
+      key: UniqueKey(),
       urlToFetch: "/post/feed",
+      extraUrlData: {"pageFetching": pageOpen},
       widgetAddedToTop: Center(
           child: Column(children: [
-        SizedBox(height: 32),
-        Padding(
+        const SizedBox(height: 32),
+        const Padding(
             padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16),
             child: Text(
               "Your feed",
               style: TextStyle(color: Colors.white, fontSize: 40),
             )),
-        Divider(
+        const Divider(
           color: Color.fromARGB(255, 110, 110, 110),
           thickness: 1.0,
+        ),
+        Padding(
+          //share mode selection
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Container(
+            width: double.infinity,
+            child: ToggleSwitch(
+              minWidth: double.infinity,
+              cornerRadius: 15.0,
+              initialLabelIndex: pageItemIndex,
+              totalSwitches: 2,
+              activeBgColors: const [
+                [Colors.green],
+                [Colors.green]
+              ],
+              centerText: true,
+              activeFgColor: Colors.white,
+              inactiveBgColor: const Color.fromARGB(255, 40, 40, 40),
+              inactiveFgColor: Colors.white,
+              labels: ['popular posts', 'followers posts'],
+              onToggle: changePageOpen,
+            ),
+          ),
         ),
         SizedBox(
           height: 16,
