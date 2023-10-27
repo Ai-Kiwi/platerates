40d8d34be4bfbd877e73f9a5355709bcbd3625a2
Ai-Kiwi <contact@aikiwi.dev>
2023-10-27 17:54:57 +1300

    made send server notifcation token after login

--- lib/login/userLogin.dart
+++ lib/login/userLogin.dart
@@ -3,7 +3,10 @@ import 'dart:convert';
 import 'package:Toaster/libs/alertSystem.dart';
 import 'package:Toaster/login/createAccount.dart';
 import 'package:Toaster/login/userResetPassword.dart';
+import 'package:Toaster/notifications/appNotificationHandler.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -231,9 +234,11 @@ class _LoginPageState extends State<LoginPage> {
                                       MaterialPageRoute(
                                           builder: (context) => MyHomePage()),
                                     );
-                                    //save
-                                    // ignore: use_build_context_synchronously
-                                    //informServerNotificationToken();
+                                    //prompt server to save token
+                                    final fcmToken = await FirebaseMessaging
+                                        .instance
+                                        .getToken();
+                                    informServerNotificationToken(fcmToken);
                                   } else {
                                     // ignore: use_build_context_synchronously
                                     openAlert("error", "invalid login",
