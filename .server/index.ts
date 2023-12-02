3b4fd1405592fe5fd121ee5949dd1336fb451125
Ai-Kiwi <contact@aikiwi.dev>
2023-12-02 15:45:50 +1300

    made slight performance improvements. changed app icon

--- .server/index.ts
+++ .server/index.ts
@@ -25,7 +25,7 @@ const limiter = rateLimit({
 require('dotenv').config();
 
 
-const clientVersion: string = "1.1.0+11";
+const clientVersion: string = "1.1.0+12";
 //make sure to update lisesnes on securityUtil file
 
 //setup app
